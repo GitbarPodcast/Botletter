c36f45e333d0929a3caf031cbe72d2f7064f9990
brainrepo <murru7@gmail.com>
2021-06-15 17:25:36 +0200

    feat(renderer): add tests

---
+++ src/core/templating/__test__/index.test.ts
@@ -0,0 +1,12 @@
+import renderer from '..';
+import index from '../../../assets/htmlTemplate';
+
+describe('templating', () => {
+  it('it should render the template', async () => {
+    const html = renderer(
+      { articles: [], episode: { title: 'titolo', description: 'desc', image: 'image url', link: 'link_url' } },
+      index as React.FC,
+    );
+    expect(html).toMatchSnapshot();
+  });
+});
