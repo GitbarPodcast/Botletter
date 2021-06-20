1e612a4e5281b9c6e9bd642fd092016e4cf0f434
Luca <luca.rainone@gmail.com>
2021-06-20 22:30:30 +0200

    fix: use undiciRequest instead of deprecated fakeRequest
    We don't need fakeRequest

--- src/infrastructure/delivery/index.ts
+++ src/infrastructure/delivery/index.ts
@@ -1,6 +1,6 @@
 import { providerFactory } from './provider/entities';
 import { Mail } from './provider/Mail';
-import fakeRquest from '../network/fakeRequest';
+import request from '../network/undiciRequest';
 const PROVIDER = process.env.MAIL_PROVIDER || 'Mailchimp';
 const PROVIDER_CONFIG = process.env.MAIL_PROVIDER_CONFIG || 'missed_config';
 const FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS || 'info@test.it';
@@ -16,7 +16,7 @@ mail.setSubject('newsletter test').setFrom({ address: FROM_ADDRESS, name: FROM_N
   const sender = await providerFactory(PROVIDER, PROVIDER_CONFIG);
 
   try {
-    const response = await sender.send(mail, fakeRquest);
+    const response = await sender.send(mail, request);
     console.log(response);
   } catch (e) {
     console.error('Send failed: ' + e.toString());
