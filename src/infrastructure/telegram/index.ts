ecd545d04dde9178e25b8497555690144b01514e
brainrepo <murru7@gmail.com>
2021-06-27 17:19:42 +0200

    wip(telegram): integrate carmine's work

---
+++ src/infrastructure/telegram/index.ts
@@ -0,0 +1,33 @@
+// Message represent a message sent to the bot
+// it's an abstraction on the full message struct. see https://core.telegram.org/bots/api#messageentity
+
+// This number may have more than 32 significant bits and some programming languages may have
+// difficulty/silent defects in interpreting it.
+// But it has at most 52 significant bits, so a signed 64-bit integer or double-precision
+// float type are safe for storing this identifier
+export type TelegramID = number;
+
+export interface User {
+  id?: TelegramID;
+  username?: string;
+}
+
+export interface Chat {
+  id: TelegramID;
+  title?: string;
+}
+
+export interface Message {
+  id: TelegramID;
+  from: User;
+  chat: Chat;
+  date: string; // iso8601
+  hashtags: string[];
+  urls: string[];
+  text: string;
+}
+
+export const messageHasHashtags = (message: Message): boolean => message.hashtags.length > 0;
+export const messageHasUrls = (message: Message): boolean => message.urls.length > 0;
+export const messageIsFromRecognizedAuthor = (message: Message, author: TelegramID): boolean =>
+  message.from.id === author;
---
+++ src/infrastructure/telegram/normalizeMessages.ts
@@ -0,0 +1,32 @@
+import { Message as apiMessage, MessageEntity } from 'typegram';
+import { Message } from './index';
+
+const extractEntitiesFromText =
+  (text: string) =>
+  (e: MessageEntity): string =>
+    text.substring(e.offset, e.offset + e.length);
+
+export const normalizeTelegramMessage = (apiMessage: apiMessage.TextMessage): Message => {
+  const hashtags = (apiMessage.entities || [])
+    .filter((e) => e.type === 'hashtag')
+    .map(extractEntitiesFromText(apiMessage.text));
+  const urls = (apiMessage.entities || [])
+    .filter((e) => e.type === 'url')
+    .map(extractEntitiesFromText(apiMessage.text));
+
+  return {
+    id: apiMessage.message_id,
+    chat: {
+      id: apiMessage.chat.id,
+      title: apiMessage.chat.type === 'group' ? apiMessage.chat.title : undefined,
+    },
+    urls: urls,
+    hashtags: hashtags,
+    from: {
+      id: apiMessage.from?.id,
+      username: apiMessage.from?.username,
+    },
+    date: new Date(apiMessage.date).toISOString(),
+    text: apiMessage.text,
+  };
+};
